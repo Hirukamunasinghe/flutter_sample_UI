bb87ca3e14807e68d9b45594ebf8ac96175e30c7
Hirukamunasinghe <munasinghehiruka@gmail.com>
2022-09-19 23:34:37 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/new_home_page_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.new_home_page_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        leading: Icon(Icons.account_balance),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.black,
+        child: Center(
+          child: Text('NETFLIX', style: TextStyle(color: Colors.red, fontSize: 50,fontWeight: FontWeight.bold),),
+        ),
+      ),
+    );
+  }
+}
